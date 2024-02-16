e09b494965f14ffe2d840ed1744383f3316cb4fd
Kuasawan-Murbawan <davidafiq@gmail.com>
2024-02-17 04:51:37 +0800

    directives and mouse events

--- app.js
+++ app.js
@@ -6,10 +6,39 @@ const app = Vue.createApp({
     // data is a function that returns an object
 
     return {
+      showBooks: true,
       title: "The final empire",
       author: "Brandon Sanderson",
       age: 45,
+      x: 0,
+      y: 0,
     };
+  },
+
+  methods: {
+    changeAuthor1() {
+      this.author = "Micheal Jackson";
+    },
+
+    changeAuthor(author) {
+      this.author = author;
+    },
+
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+
+    handleEvent(event, data) {
+      console.log(event, event.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+
+    handleMouseMove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY; // get the current position of cursor
+    },
   },
 });
 
